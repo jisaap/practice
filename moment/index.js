da63ddaa6234447558dacc7a2d7a81bce4cb5857
jisaap <52190253+jisaap@users.noreply.github.com>
2020-04-10 20:10:12 +0900

    Update index.js

--- moment/index.js
+++ moment/index.js
@@ -6,7 +6,6 @@ const CLICKED_CLASS = "clicked";
 
 function handleClick() {
    
-   
     // toggle 은 함수안에 있는 값을 체크해 클래스가 있으면 remove 없으면 add 해줌
     title.classList.toggle(CLICKED_CLASS);
     // 같은 역할을 하는 토글로 대체함
